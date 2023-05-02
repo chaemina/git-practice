1a8fd4a153cba7024521b3e8e7572099017961eb
chaemina <chaemina82@naver.com>
2023-05-03 02:29:05 +0900

    feat: Python env test
    Create do_fizzbuzz and test

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def do_fizzbuzz():
+    """
+    fizzbuzz : print fizz buzz fuzzbuzz
+    3: fizz
+    5: buzz
+    15:fuzzbuzz
+    etc: num
+    """
+    print('hello')
+
+if __name__=='__main__':
+    do_fizzbuzz()
